b87d70c2f428e0dbf1c11b3a8790a4e3e5c093ef
mAmineChniti <emin.chniti@esprit.tn>
2023-07-09 18:34:06 +0100

    fixing AI

--- micro/services.py
+++ micro/services.py
@@ -4,18 +4,24 @@ nlp = spacy.load("en_core_web_sm")
 def process_card(text):
     doc = nlp(text)
     
-    result = {}
+    result = {
+        "name": None,
+        "phone_number": None,
+        "location": None,
+        "occupation": None,
+        "other_details": None
+    }
     
     for entity in doc.ents:
-        if entity.label_ == "PERSON":
+        if entity.label_ == "PERSON" and not result["name"]:
             result["name"] = entity.text
-        elif entity.label_ == "PHONE_NUMBER":
+        elif entity.label_ == "PHONE_NUMBER" and not result["phone_number"]:
             result["phone_number"] = entity.text
-        elif entity.label_ == "GPE":
+        elif entity.label_ == "GPE" and not result["location"]:
             result["location"] = entity.text
-        elif entity.label_ == "OCCUPATION":
+        elif entity.label_ == "OCCUPATION" and not result["occupation"]:
             result["occupation"] = entity.text
-        else:
+        elif not result["other_details"]:
             result["other_details"] = entity.text
     
     return result
