47fb4457242ce5618bb896941eca256f96c95a2d
mAmineChniti <emin.chniti@esprit.tn>
2023-07-09 21:01:47 +0100

    AI is tough

--- micro/services.py
+++ micro/services.py
@@ -1,40 +1,22 @@
 import spacy
 
-def process_card(input_text):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(input_text)
+nlp = spacy.load("en_core_web_lg")
+
+def process_card(text):
+    doc = nlp(text)
     
-    result = {
-        "name": None,
-        "phone_number": None,
-        "location": None,
-        "occupation": None,
-        "other_details": None
-    }
+    result = {}
     
-    entities = [ent.text for ent in doc.ents]
-    for entity in entities:
-        if result["name"] is None and entity.istitle():
-            result["name"] = entity
-        elif result["phone_number"] is None and entity.startswith(("(", "+")):
-            result["phone_number"] = entity
-        elif result["location"] is None and entity.endswith(("Street", "Avenue")):
-            result["location"] = entity
-        elif result["occupation"] is None and "Manager" in entity:
-            result["occupation"] = entity
-    
-    details = []
-    for token in doc:
-        if token.pos_ == "NUM":
-            details.append(token.text)
-        elif token.pos_ == "PROPN":
-            details.append(token.text)
-        elif token.like_email:
-            details.append(token.text)
-        elif token.like_url:
-            details.append(token.text)
-    
-    if details:
-        result["other_details"] = "\n".join(details)
+    for entity in doc.ents:
+        if entity.label_ == "PERSON":
+            result["name"] = entity.text
+        elif entity.label_ == "PHONE_NUMBER":
+            result["phone_number"] = entity.text
+        elif entity.label_ == "GPE":
+            result["location"] = entity.text
+        elif entity.label_ == "OCCUPATION":
+            result["occupation"] = entity.text
+        else:
+            result["other_details"] = entity.text
     
     return result
