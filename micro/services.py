1fb0b33a0e80e33095b41d6c99bedb97c604d70d
mAmineChniti <emin.chniti@esprit.tn>
2023-07-10 21:12:25 +0100

    more accurate other details column

--- micro/services.py
+++ micro/services.py
@@ -5,7 +5,13 @@ nlp = spacy.load("../custom_model")
 def process_card(text):
     doc = nlp(text)
     
-    result = {}
+    result = {
+        "name": None,
+        "phone_number": None,
+        "location": None,
+        "occupation": None,
+        "other_details": []
+    }
     
     for entity in doc.ents:
         if entity.label_ == "PERSON":
@@ -17,6 +23,8 @@ def process_card(text):
         elif entity.label_ == "OCCUPATION":
             result["occupation"] = entity.text
         else:
-            result["other_details"] = entity.text
+            result["other_details"].append(entity.text)
+    
+    result["other_details"] = ' '.join(result["other_details"])
     
     return result
