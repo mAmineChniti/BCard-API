1590b087f1b31e9517f40b16e61d2e92d94c4ad9
mAmineChniti <emin.chniti@esprit.tn>
2023-07-25 09:00:58 +0100

    reverted to no ci/cd

--- config/database.py
+++ config/database.py
@@ -1,14 +1,8 @@
 from pymongo import MongoClient
 from os import environ, path
+from dotenv import load_dotenv
 
-# Determine the environment (GitHub Actions or Render.com)
-is_github_actions = "GITHUB_ACTIONS" in environ
-dotenv_path = "/etc/secrets/.env" if is_github_actions else ".env"
-
-# Load environment variables from the appropriate .env file
-if path.exists(dotenv_path):
-    from dotenv import load_dotenv
-    load_dotenv(dotenv_path=dotenv_path)
+load_dotenv('/etc/secrets/.env')
 
 # Get the MongoDB connection details from environment variables
 username = environ.get("DBUSER")
@@ -17,9 +11,6 @@ dbname = environ.get("DBNAME")
 col_name = environ.get("COLNAME")
 
 # Connect to MongoDB using the retrieved connection details
-if username and password and dbname and col_name:
-    client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.taxfc6l.mongodb.net/?retryWrites=true&w=majority")
-    db = client[dbname]
-    collection_name = db[col_name]
-else:
-    raise ValueError("MongoDB connection details not provided in environment variables.")
+client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.taxfc6l.mongodb.net/?retryWrites=true&w=majority")
+db = client[dbname]
+collection_name = db[col_name]
