82c9f9a2e4861c41cb6aa4b0d6dc2948e90fbe53
mAmineChniti <emin.chniti@esprit.tn>
2023-07-25 13:19:05 +0100

    added async

--- routes/bcardroute.py
+++ routes/bcardroute.py
@@ -4,33 +4,37 @@ from micro.services import process_card
 from config.database import bcards_collection
 from config.cache import redis_client
 from schemas.cardschema import cards_serialize
+import asyncio
 
 BCardrouter = APIRouter()
 
 # Redirection to documentation page
 @BCardrouter.get("/", include_in_schema=False)
-async def redirect_to_docs():
+def redirect_to_docs():
     return RedirectResponse(url='/docs')
 
 # API Status check
 @BCardrouter.get("/status", include_in_schema=False)
 async def status():
-    return {"status": "ok"}
+    try:
+        return {"status": "ok"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 # Save text from front in a MongoDB database
 @BCardrouter.post('/save_card')
-def save_card(user_id: str, text: str):
+async def save_card(user_id: str, text: str):
     try:
-        result = process_card(text)
+        result = await process_card(text)
 
         # Add user_id to the result dictionary
         result["_id"] = user_id
 
         # Store the entire result dictionary in MongoDB
-        bcards_collection.insert_one(result)
+        await bcards_collection.insert_one(result)
 
         # Invalidate the cache for the user_id, as the data has changed
-        redis_client.delete(user_id)
+        await redis_client.delete(user_id)
 
         return {"message": "Card saved successfully"}
     except Exception as e:
@@ -39,21 +43,21 @@ def save_card(user_id: str, text: str):
 
 # Route to get all cards of a user with Redis caching
 @BCardrouter.get('/cards')
-def user_cards(user_id: str):
+async def user_cards(user_id: str):
     try:
         # Check if the data is already cached in Redis
-        cached_data = redis_client.get(user_id)
+        cached_data = await redis_client.get(user_id)
         if cached_data:
             return JSONResponse(content=cached_data)
 
         # Query MongoDB collection for cards of the specified user_id
-        cards = bcards_collection.find({"_id": user_id})
+        cards = await bcards_collection.find({"_id": user_id})
 
         # Serialize the cards
         serialized_cards = cards_serialize(cards)
-        
+
         # Cache the data in Redis for future requests
-        redis_client.set(user_id, serialized_cards)
+        await redis_client.set(user_id, serialized_cards)
 
         return JSONResponse(content=serialized_cards)
     except Exception as e:
