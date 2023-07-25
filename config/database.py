aa7247fe73d1f0fd5f6da568765f5db8eaf875ee
mAmineChniti <emin.chniti@esprit.tn>
2023-07-25 13:32:26 +0100

    added async to database to avoid bottleneck

--- config/database.py
+++ config/database.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from os import environ
 from dotenv import load_dotenv
 from pathlib import Path
+from motor.motor_asyncio import AsyncIOMotorClient
 
 dotenv_path = Path('/etc/secrets/.env')
 load_dotenv(dotenv_path=dotenv_path)
@@ -14,6 +15,6 @@ collection = environ.get("COLLECTION")
 host = environ.get("HOST")
 
 # Connect to MongoDB using the retrieved connection details
-client = MongoClient(f"mongodb+srv://{username}:{password}@{host}")
+client = AsyncIOMotorClient(f"mongodb+srv://{username}:{password}@{host}")
 db = client[database]
 bcards_collection = db[collection]
